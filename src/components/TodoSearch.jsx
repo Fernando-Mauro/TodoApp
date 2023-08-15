1d1c037a9f026133a7321f8493b524662c12683c
Fernando Mauro <fermaurolf@gmail.com>
2023-08-15 14:57:17 -0600

    Clase 7 terminada

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import "./TodoSearch.css"
 
 export const TodoSearch = () => {
+  const [inputvalue, setInputValue] = useState("");
+
   return (
-    <input placeholder="Cortar cabello 👱"
+    <input
+      placeholder="Cortar cabello 👨‍🦲"
       className="TodoSearch"
+      value={inputvalue}
+      
+      onChange={({ target }) => {
+        setInputValue(target.value);
+      }}
     />
   )
 }
